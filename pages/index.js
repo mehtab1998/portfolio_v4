033d376e90c8f2483c532673951d8b33f88ee257
Mehtab <mehtab@Mehtabs-MacBook-Pro.local>
2022-10-31 23:11:12 -0700

    test

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-import Head from 'next/head'
 import About from '../components/About'
 import Contact from '../components/Contact'
 import Main from '../components/Main'
@@ -9,11 +8,6 @@ import FavIcon from '../public/assets/MR.png'
 export default function Home() {
   return (
     <div>
-      <Head>
-        <title>Mehtab | Front-End Developer</title>
-        <meta name="description" content="I’m a front-end web developer specializing in building (and occasionally designing) exceptional digital experiences." />
-        <link rel="icon" href={FavIcon} />
-      </Head>
     <Main />
     <About />
     <Skills />
--- pages/resume.jsx
+++ pages/resume.jsx
@@ -1,19 +1,9 @@
 import React from 'react';
-import Head from 'next/head';
 import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
 
 const resume = () => {
   return (
     <>
-      <Head>
-        <title>Mehtab | Resume</title>
-        <meta
-          name='description'
-          content='I&apos;m a front-end web developer specializing in building (and occasionally designing) exceptional digital experiences.'
-        />
-        <link rel='icon' href='/fav.png' />
-      </Head>
-
       <div className='max-w-[940px] mx-auto p-2 pt-[120px]'>
         <h2 className='text-center'>Resume</h2>
         <div className='bg-[#d0d4d6] my-4 p-4 w-full flex justify-between items-center'>
